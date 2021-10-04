915af36e4d7da8b5c4a6ceaabcdc99ef15210936
Cristobal Torres <cristobal_javier@hotmail.com>
2021-10-04 16:25:13 -0300

    nbspider working with xpath

--- scrapy/nbscraper/nbscraper/items.py
+++ scrapy/nbscraper/nbscraper/items.py
@@ -4,9 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
+def remove_currency(value):
+    return value.replace(["$", ".", ","], "").strip()
+
+def clean_string(string):
+    return string.replace(["\n", "\r", "\t"], "").strip()
 
 class NbscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     pass
--- scrapy/nbscraper/nbscraper/spiders/nbspider.py
+++ scrapy/nbscraper/nbscraper/spiders/nbspider.py
@@ -5,28 +5,27 @@ class NBSpider(scrapy.Spider):
 	start_urls = ["https://www.solotodo.cl/notebooks?ordering=offer_price_usd&"]
 
 	def parse(self, response):
-		for products in response.css("div.d-flex.flex-column.category-browse-result"):
+		for nb in response.xpath("//div[@class='d-flex flex-column category-browse-result']"):
 			try:
 				yield {
-					"name": products.css("div.d-flex.flex-column.category-browse-result a::text").get(),
-					"price": products.css("div.d-flex.flex-column.category-browse-result div.price.flex-grow a::text").get(),
-					"processor": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[0],
-					"ram": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[1],
-					"monitor": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[2],
-					"storage": products.css("div.d-flex.flex-column.category-browse-result div.description-container li::text").getall()[0],
-					"video-card": products.css("div.d-flex.flex-column.category-browse-result div.description-container li::text").getall()[1],
+					"name": nb.xpath(".//h3/a/text()").get(),
+					"price": nb.xpath(".//div[@class='d-flex flex-row justify-content-between align-items-center mt-auto pt-2']/div/a/text()").get(),
+					"processor": nb.xpath(".//div[@class='description-container']/dl/dd[1]/text()").get(),
+					"ram": nb.xpath(".//div[@class='description-container']/dl/dd[2]/text()").get(),
+					"monitor": nb.xpath(".//div[@class='description-container']/dl/dd[3]/text()").get(),
+					"storage": nb.xpath(".//div[@class='description-container']/dl/dd[4]/ul/li/text()").get(),
+					"video-card": nb.xpath(".//div[@class='description-container']/dl/dd[5]/ul/li/text()").get(),
 				}
 			except:
 				yield {
-					"name": products.css("div.d-flex.flex-column.category-browse-result a::text").get(),
-					"price": products.css("div.d-flex.flex-column.category-browse-result div.price.flex-grow a::text").get(),
-					"processor": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[0],
-					"ram": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[1],
-					"monitor": products.css("div.d-flex.flex-column.category-browse-result div.description-container dd::text").getall()[2],
-					"storage": products.css("div.d-flex.flex-column.category-browse-result div.description-container li::text").getall()[0],
+					"name": nb.xpath(".//h3/a/text()").get(),
+					"price": nb.xpath(".//div[@class='d-flex flex-row justify-content-between align-items-center mt-auto pt-2']/div/a/text()").get(),
+					"processor": nb.xpath(".//div[@class='description-container']/dl/dd[1]/text()").get(),
+					"ram": nb.xpath(".//div[@class='description-container']/dl/dd[2]/text()").get(),
+					"monitor": nb.xpath(".//div[@class='description-container']/dl/dd[3]/text()").get(),
+					"storage": nb.xpath(".//div[@class='description-container']/dl/dd[4]/ul/li/text()").get(),
 					"video-card": "NA",
 				}
-		next_page = "https://www.solotodo.cl/notebooks" + response.css("a.page-link::attr(href)").getall()[-1]
-		if next_page is not None:
-			yield response.follow(next_page, callback=self.parse)
-
+#		next_page = "https://www.solotodo.cl/notebooks" + response.css("a.page-link::attr(href)").getall()[-1]
+#		if next_page is not None:
+#			yield response.follow(next_page, callback=self.parse)
